00c321eb832512f3f5278da2d20b966853687891
Gabriel Eze <gbrjnr@outlook.com>
2021-08-31 16:48:43 +0100

    Working on C functions-nested_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
+ * main - Entry point
  * main -  prints Holberton, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char ch  = "Holberton";
+
+	while (ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
 	_putchar('\n');
 
 	return (0);
