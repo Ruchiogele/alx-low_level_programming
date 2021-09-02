11cb32912dd48cde399784c0db026e87fe3900a0
Gabriel Eze <gbrjnr@outlook.com>
2021-09-02 15:40:13 +0100

    Working with more_functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-  * print_numbers - Print the numbers from 0 - 9 followed by a new line
+  * print_numbers - Print the numbers since 0 up to 9
   *
-  * Return: The numbers from 0 - 9
+  * Return: The numbers since 0 up to 9
   */
-
 void print_numbers(void)
 {
 	int x;
