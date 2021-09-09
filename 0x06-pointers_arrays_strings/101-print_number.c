04c6a71de65688e13368a15db70dfbd35ab0c347
Gabriel Eze <gbrjnr@outlook.com>
2021-09-09 18:17:06 +0100

    Advanced,C function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,29 @@
+#include <stdio.h>
 #include "main.h"
-
-/**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
- */
 
 void print_number(int n)
 {
-	int num = n;
-
-	if (n < 0)
+    // If number is smaller than 0, put a - sign
+    // and change number to positive
+	if (n < 0) 
 	{
-		_putchar('-');
-		num = -num;
-	}
-
-	else if ((num / 10) > 0)
-	{
-		print_number(num / 10);
-		_putchar((num % 10) + '0');
-	}
+        	putchar('-');
+        	n = -n;
+    	}
+  
+    // Remove the last digit and recur
+    	if (n/10)
+        	print(n/10);
+  
+    // Print the last digit
+    	putchar(n%10 + '0');
+}
+  
+// Driver program to test abvoe function
+int main(void)
+{
+    	int n = 12045;
+    
+    		print(n);
+    	return 0;
 }
