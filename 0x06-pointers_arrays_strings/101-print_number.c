0209efc994a715883fe815d565cd387a2f5004e2
Gabriel Eze <gbrjnr@outlook.com>
2021-09-09 19:15:54 +0100

    function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,18 @@
  */
 void print_number(int n)
 {
-	/*If number is smaller than 0, put a - sign and change number to positive*/
+   	 // If number is smaller than 0, put a - sign 
+    	// and change number to positive 
 	if (n < 0) 
 	{ 
-        	putchar('-'); 
+        	putchar ('-'); 
         	n = -n; 
 	} 
   
-	/*Remove the last digit and recur*/
+    // Remove the last digit and recur 
     	if (n / 10) 
         	print_number(n / 10); 
   
-	/*Print the last digit*/ 
+    // Print the last digit 
     	putchar(n % 10 + '0'); 
 }
