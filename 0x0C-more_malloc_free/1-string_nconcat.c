c9fea7ba6d57a1c6608213a7f4faf1e0879b894b
Gabriel Eze <gbrjnr@outlook.com>
2021-09-24 13:08:13 +0100

    Added: 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,48 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * string_nconcat - concatenate two strings
- *
- * @s1: is a pointer to the first string
- * @s2: is a pointer to the second string
- * @n: an integer (6) with the limit of what we are going to add to s1
- *
- * Return: pointer to a newlly allocated memory with s1 & s2, NULL if it fail
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: number of bytes.
+ * Return: pointer to the allocated memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	/* unsigned int var because we need to compare with n */
-	char *strings;
-	unsigned int lengths1, lengths2;
+	char *sout;
+	unsigned int a1, a2, aout, i;
 
-	/* task condition if NULL passed, should be treated as empty string */
-	/* same condition of project 0x0B 2-str.concatena */
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	/* I go through both strings so I can find their length */
-	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
-	{
-	}
-	for (lengths2 = 0; s2[lengths2] != '\0'; lengths2++)
-	{
-	}
-	/* Now that I know the length of s2 I can compare it with n */
-	/* and set the task condition */
-	if (n > lengths2)
-		n = lengths2;
-	/* now set the malloc with the lenghts */
-	/* n is now equal to lengths2 */
-	/* plus 1 because we need to save space for the last '\0' of strings */
-	strings = malloc((lengths1 + n + 1) * sizeof(char));
-	if (strings == NULL)
+
+	for (a1 = 0; s1[a1] != '\0'; a1++)
+		;
+
+	for (a2 = 0; s2[a2] != '\0'; a2++)
+		;
+
+	if (n >= a2)
+		n = a2;
+
+	aout = a1 + n;
+
+	sout = malloc(aout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	/* now we add the content of strings to new strings pointer */
-	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
-		strings[lengths1] = s1[lengths1];
-	/* iterate while lengths2 < n so we print only school */
-	/* lenghts1 keeps counting while we go trough s2 */
-	for (lengths2 = 0; lengths2 < n; lengths2++, lengths1++)
-	strings[lengths1] = s2[lengths2];
-	strings[lengths1] = '\0';
-	return (strings);
+
+	for (i = 0; i < aout; i++)
+		if (i < a1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - a1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
