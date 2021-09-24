0775a5fb15ca12b768f374881f688518d21fb705
Gabriel Eze <gbrjnr@outlook.com>
2021-09-24 13:03:42 +0100

    Added: 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,51 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - concatenate two strings
+ *
+ * @s1: is a pointer to the first string
+ * @s2: is a pointer to the second string
+ * @n: an integer (6) with the limit of what we are going to add to s1
+ *
+ * Return: pointer to a newlly allocated memory with s1 & s2, NULL if it fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	/* unsigned int var because we need to compare with n */
+	char *strings;
+	unsigned int lengths1, lengths2;
 
+	/* task condition if NULL passed, should be treated as empty string */
+	/* same condition of project 0x0B 2-str.concatena */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-	while (j < l)
+	/* I go through both strings so I can find their length */
+	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
 	{
-		if (j <= i)
-			str[j] = s1[j];
-
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
 	}
-	str[j] = '\0';
-	return (str);
+	for (lengths2 = 0; s2[lengths2] != '\0'; lengths2++)
+	{
+	}
+	/* Now that I know the length of s2 I can compare it with n */
+	/* and set the task condition */
+	if (n > lengths2)
+		n = lengths2;
+	/* now set the malloc with the lenghts */
+	/* n is now equal to lengths2 */
+	/* plus 1 because we need to save space for the last '\0' of strings */
+	strings = malloc((lengths1 + n + 1) * sizeof(char));
+	if (strings == NULL)
+		return (NULL);
+	/* now we add the content of strings to new strings pointer */
+	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
+		strings[lengths1] = s1[lengths1];
+	/* iterate while lengths2 < n so we print only school */
+	/* lenghts1 keeps counting while we go trough s2 */
+	for (lengths2 = 0; lengths2 < n; lengths2++, lengths1++)
+	strings[lengths1] = s2[lengths2];
+	strings[lengths1] = '\0';
+	return (strings);
 }
