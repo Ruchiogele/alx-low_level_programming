cada4cdee96e948d981445bceb1ae7a37b664936
Gabriel Eze <gbrjnr@outlook.com>
2021-10-18 21:25:18 +0100

    Added the main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void ensure_endianness(char little_e, void *data, unsigned int len);
+void print_elf_header(void *h);
+void print_elf_header_2(void *h);
+void put_err(char *str);
 
 #endif /* MAIN_H */
