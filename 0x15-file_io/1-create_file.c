bd6f2717fe5b3b9f9c9cd4ae392c443978c810c1
Gabriel Eze <gbrjnr@outlook.com>
2021-10-18 21:34:15 +0100

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,30 @@
 #include "main.h"
 
 /**
- * create_file - Function that creates a file.
- * @filename: The name of the file to create.
- * @text_content: Is a string to write to the file.
- * Return: 1 on success, -1 on failure.
+ * create_file - Creates a file
+ * @filename: Name of file to be created
+ * @text_content: Content of the file
+ *
+ * Return: 1 0n success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int i;
+	int fd, rcheck, x;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-	if (fd < 0)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
-			if (fd == -1)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		if (write(fd, &text_content[i], 1) == -1)
+		for (x = 0; text_content[x] != '\0'; x++)
+			;
+		rcheck = write(fd, text_content, x);
+		if (rcheck == -1)
 			return (-1);
 	}
 	close(fd);
